fb2bf21562e6b7fd7a3ca7f21af5d28774d0e3ec
babhero10 <abdallah.elsaid25@gmail.com>
2024-11-19 14:25:54 +0200

    Add todo

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,7 +1,10 @@
+-- Spaces and editor configurations.
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+-- Key mapping
 vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
