55db2edab8f7c90f038051266576818c3e29e866
babhero10 <abdallah.elsaid25@gmail.com>
2024-11-19 20:32:08 +0200

    Add git plugins

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 require("vim-options")
 require("config.lazy")
+
+-- Alpha nvim setup (conditional loading)
+local alpha = require'alpha'
+local startify = require'alpha.themes.startify'
+
+-- Check if the directory or file is passed as an argument
+if #vim.fn.argv() == 0 or vim.fn.isdirectory(vim.fn.argv()[1]) == 1 then
+  -- Show alpha if no file or folder is passed
+  alpha.setup(startify.config)
+else
+  -- Otherwise, proceed without Alpha (handle folder or file opening)
+  -- For example, initialize your folder plugin (like nvim-tree or telescope)
+  -- require'nvim-tree'.setup()
+end
