b50a8ff19d585b3ae60abc4d39b5a22c2dca918e
babhero10 <abdallah.elsaid25@gmail.com>
2024-11-19 07:35:34 +0200

    Add autocompletetion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,61 +1,51 @@
 return {
-  -- Mason setup
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require('mason').setup()
-    end
-  },
+	-- Mason setup
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-  -- Mason LSPConfig setup
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require('mason-lspconfig').setup({
-        ensure_installed = { "lua_ls", "clangd", "pylsp", "jdtls" }  -- Corrected clang to clangd
-      })
-    end
-  },
+	-- Mason LSPConfig setup
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "clangd", "pylsp", "jdtls" }, -- Corrected clang to clangd
+			})
+		end,
+	},
 
-  -- nvim-lspconfig setup
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require('lspconfig')
+	-- nvim-lspconfig setup
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-      -- Setup lua_ls (Lua Language Server)
-      lspconfig.lua_ls.setup {
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = {'vim'}  -- Add vim to the list of recognized globals
-            },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),  -- Include Neovim runtime files
-            }
-          }
-        }
-      }
+			-- Setup lua_ls (Lua Language Server)
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
 
-      -- Setup clangd (C/C++ Language Server)
-      lspconfig.clangd.setup {}
+			-- Setup clangd (C/C++ Language Server)
+			lspconfig.clangd.setup({ capabilities = capabilities })
 
-      -- Setup pylsp (Python Language Server)
-      lspconfig.pylsp.setup {}
+			-- Setup pylsp (Python Language Server)
+			lspconfig.pylsp.setup({ capabilities = capabilities })
 
-      -- Setup jdtls (Java Language Server)
-      lspconfig.jdtls.setup {}
+			-- Setup jdtls (Java Language Server)
+			lspconfig.jdtls.setup({ capabilities = capabilities })
 
-      -- Setup keybindings for LSP directly
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)  -- Go to declaration
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition)    -- Go to definition
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover)           -- Hover
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)  -- Go to implementation
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help) -- Signature help
-      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)  -- Rename
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action) -- Code action
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references)      -- Go to references
-      vim.keymap.set('n', '<leader>f', vim.lsp.buf.format) -- Format buffer
-    end
-  }
+			-- Setup keybindings for LSP directly
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration) -- Go to declaration
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition) -- Go to definition
+			vim.keymap.set("n", "K", vim.lsp.buf.hover) -- Hover
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation) -- Go to implementation
+			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help) -- Signature help
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename) -- Rename
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action) -- Code action
+			vim.keymap.set("n", "gr", vim.lsp.buf.references) -- Go to references
+			vim.keymap.set("n", "<leader>f", vim.lsp.buf.format) -- Format buffer
+		end,
+	},
 }
