6b4dbaa26c69e06f2a3c7375197e783386fe217d
babhero10 <abdallah.elsaid25@gmail.com>
2024-11-19 14:29:10 +0200

    Add keybinddings to todo

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -4,6 +4,7 @@ return {
   requires = 'nvim-lua/plenary.nvim',
   config = function()
     require("todo-comments").setup {}
+    vim.api.nvim_set_keymap('n', '<leader>nt', '<cmd>TodoNext<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>pt', '<cmd>TodoPrev<CR>', { noremap = true, silent = true })
   end
-
 }
